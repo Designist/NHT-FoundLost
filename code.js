65070f25983864e4134ad04ed8271f42bb3ee75f
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2017-11-30 16:01:28 -0500

    final audio controls update

--- code.js
+++ code.js
@@ -2,6 +2,7 @@ window.onload = function() {
     if (window.jQuery) {  
         // jQuery is loaded 
         var currentstory;
+
         audiojs.events.ready(function() {
 		    var as = audiojs.createAll();
 
@@ -16,37 +17,39 @@ window.onload = function() {
 				$(currentstory).attr("src", "play.png");
 				$(currentstory)[0].classList.add('playbutton');
 				$(currentstory)[0].classList.remove('pausebutton');
+			});
+
+			var container;
+	        var audioPlayer;
+	        $(".button").click(function() {
+				if ($(this).hasClass("playbutton")) { // switch to pause
+					if (currentstory != this) {
+						var url = $(this).attr("data-url");
+						// $("#audioPlayer").attr("src", url);
+						as[0].load(url);
+					}
+					$(this).attr("src", "pause.png");
+					$(this)[0].classList.add("pausebutton");
+					$(this)[0].classList.remove("playbutton");
+					$(".audiojs").css("display", "block");
+					currentstory = this;
+					$('.audiojs').each(function(index) {
+					    container = $(this);
+					    audioPlayer = container.find('audio');
+					    audiojs.instances["audiojs0"].play();
+					});
+					
+				} else if ($(this).hasClass("pausebutton")) { // switch to play
+					$(this).attr("src", "play.png");
+					$(this)[0].classList.add('playbutton');
+					$(this)[0].classList.remove('pausebutton');
+					$('.audiojs').each(function(index) {
+					    audiojs.instances["audiojs0"].pause();
+					});
+				}
 			}); 
 		  });
-
-        var container;
-        var audioPlayer;
-        $(".button").click(function() {
-			if ($(this).hasClass("playbutton")) { // switch to pause
-				$(this).attr("src", "pause.png");
-				$(this)[0].classList.add("pausebutton");
-				$(this)[0].classList.remove("playbutton");
-				$(".audiojs").css("display", "block");
-				var url = $(this).attr("data-url");
-				if (currentstory != this) {
-					$("#audioPlayer").attr("src", url);
-				}
-				currentstory = this;
-				$('.audiojs').each(function(index) {
-				    container = $(this);
-				    audioPlayer = container.find('audio');
-				    audiojs.instances["audiojs0"].play();
-				});
-				
-			} else if ($(this).hasClass("pausebutton")) { // switch to play
-				$(this).attr("src", "play.png");
-				$(this)[0].classList.add('playbutton');
-				$(this)[0].classList.remove('pausebutton');
-				$('.audiojs').each(function(index) {
-				    audiojs.instances["audiojs0"].pause();
-				});
-			}
-		});
+        
     } else {
         // jQuery is not loaded
         console.log("JQuery isn't working.");
